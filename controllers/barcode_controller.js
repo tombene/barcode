de0cc086802f764609a61792bb98d44fb2483d8b
Thomas Benedict <tomtombene@gmail.com>
2018-05-12 09:59:07 -0600

    initial branch commit

--- controllers/barcode_controller.js
+++ controllers/barcode_controller.js
@@ -16,15 +16,56 @@ module.exports = function (app) {
 	// });
 
 	app.get('/', (req, res) => {
-		res.render("index");
+		res.render("login");
 	});
-	
+
+	app.get('/index', (req, res) => {
+		res.render("index");
+	})
+
 	app.get('/login', (req, res) => {
 		res.render("login");
 	});
 
-	app.get('/login', (req,res) => {
-		res.render("login");
+	app.post('/authenticate', function (req, res) {
+
+		// find the user
+		User.findOne({
+			name: req.body.name
+		}, function (err, user) {
+
+			if (err) throw err;
+
+			if (!user) {
+				res.json({ success: false, message: 'Authentication failed. User not found.' });
+			} else if (user) {
+
+				// check if password matches
+				if (user.password != req.body.password) {
+					res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+				} else {
+
+					// if user is found and password is right
+					// create a token with only our given payload
+					// we don't want to pass in the entire user since that has the password
+					const payload = {
+						admin: user.admin
+					};
+					var token = jwt.sign(payload, app.get('superSecret'), {
+						expiresInMinutes: 1440 // expires in 24 hours
+					});
+
+					// return the information including token as JSON
+					res.json({
+						success: true,
+						message: 'Enjoy your token!',
+						token: token
+					});
+				}
+
+			}
+
+		});
 	});
 
 	app.post('/login', (req, res) => {
