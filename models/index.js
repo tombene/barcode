fa062439b458a5c6e164dbd10723f4be3bb2db66
Thomas Benedict <tomtombene@gmail.com>
2018-05-17 14:02:18 -0600

    working on index.js in models

--- models/index.js
+++ models/index.js
@@ -17,7 +17,7 @@ var opts = {
 
 if (config.use_env_variable) {
 	console.log(config.use_env_variable);
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config, opts);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config, opts);
 }
